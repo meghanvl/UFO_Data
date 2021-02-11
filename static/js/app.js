7af5eea6cc1754d83079a752c988cf3fed0abf70
meghanvl <meghanvluoma@gmail.com>
2021-02-11 10:31:02 -0600

    udpates

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
+let tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
+function createTable(weatherData)
+
+data.forEach((weatherData) => {
+   let row = tbody.append("tr");
+   Object.entries(weatherData).forEach(([key, value]) => {
+       let cell = row.append("td");
+       cell.text(value);
+   });
+});
