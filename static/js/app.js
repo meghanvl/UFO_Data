0d693519be05dc2e44476d64af02a80fe7c1f93e
meghanvl <meghanvluoma@gmail.com>
2021-02-12 12:52:04 -0600

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 let tbody = d3.select("tbody");
+let button = d3.select("#filter-btn");
 
 // append data table to webpage and add new rows for each sighting
 function createTable(data) {
@@ -16,34 +17,33 @@ function createTable(data) {
 
 // use date form to listen for events and search through the date/time column
 // select button
-// let button = d3.select("#filter-btn");
-
-// // select form
-// let form = d3.select(".form-group");
-
-// button.on("click", runEnter);
-// form.on("submit", runEnter);
-
-// // function for events
-// function runEnter() {
-//     d3.event.preventDefault();
-//     let date = d3.select("#datetime").property("value");
-//     console.log(date);
-// }
+button.on("click", handleClick);
 
 function handleClick() {
     d3.event.preventDefault();
     let date = d3.select("#datetime").property("value");
-    let filteredDate = tableData;
+    // let filteredDate = tableData;
     
     if (date) {
-        filterDate = filteredDate.filter((row) => row.datetime === date);
+        filterDate = tableData.filter(row => row.datetime === date);
     }
 
-    createTable(filteredDate);
+    createTable(filterDate);
 }
 
-d3.selectAll("filter-btn").on("click", handleClick);
+function handleClick() {
+    d3.event.preventDefault();
+    let city = d3.select("#city").property("value");
+    // let filteredCity = tableData;
+    
+    if (city) {
+        filterCity = tableData.filter(row => row.city === city);
+    }
+
+    createTable(filterCity);
+}
+
+
 createTable(tableData);
 
 
