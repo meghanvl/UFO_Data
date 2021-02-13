797d6518e43e1485a8323e2152e5f55848f53229
meghanvl <meghanvluoma@gmail.com>
2021-02-13 11:42:50 -0600

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
-let tableData = data;
-let tbody = d3.select("tbody");
-let button = d3.select("#filter-btn");
+const tableData = data;
+const tbody = d3.select("tbody");
+const button = d3.select("#filter-btn");
 
 // append data table to webpage 
 function displayTable(data) {
@@ -18,28 +18,28 @@ function displayTable(data) {
 displayTable(tableData);
 
 // listen for events and search through the columns
-let dateInput = d3.select("#datetime");
-let cityInput = d3.select("#city");
-let stateInput = d3.select("#state");
-let countryInput = d3.select("#country");
-let shapeInput = d3.select("#shape");
+const dateInput = d3.select("#datetime");
+const cityInput = d3.select("#city");
+const stateInput = d3.select("#state");
+const countryInput = d3.select("#country");
+const shapeInput = d3.select("#shape");
 
 function handleClick() {
     // prevents page from being refreshed
     d3.event.preventDefault();
     // display new table based on filtered date
-    let dateTable = tableData.filter(row => row.datetime === dateInput.property("value"))
-    let cityTable = tableData.filter(row => row.city === cityInput.property("value"))
-    let stateTable = tableData.filter(row => row.state === stateInput.property("value"))
-    let countryTable = tableData.filter(row => row.country === countryInput.property("value"))
-    let shapeTable = tableData.filter(row => row.shape === shapeInput.property("value"))
+    const dateTable = tableData.filter(row => row.datetime === dateInput.property("value"))
+    const cityTable = tableData.filter(row => row.city === cityInput.property("value"))
+    // let stateTable = tableData.filter(row => row.state === stateInput.property("value"))
+    // let countryTable = tableData.filter(row => row.country === countryInput.property("value"))
+    // let shapeTable = tableData.filter(row => row.shape === shapeInput.property("value"))
     // display filtered table
     displayTable(dateTable);
     displayTable(cityTable);
-    displayTable(stateTable);
-    displayTable(countryTable);
-    displayTable(shapeTable);
-}
+    // displayTable(stateTable);
+    // displayTable(countryTable);
+    // displayTable(shapeTable);
+};
 
 button.on("click", handleClick);
 
